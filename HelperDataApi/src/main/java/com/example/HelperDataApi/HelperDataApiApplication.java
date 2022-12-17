d72b4b7f8cc3d5a5bd18f7cf872dbe0c0708c182
Prashant Gupta <pk.pupri@gmail.com>
2022-12-17 16:11:04 +0530

    project started

---
+++ HelperDataApi/src/main/java/com/example/HelperDataApi/HelperDataApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.HelperDataApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelperDataApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelperDataApiApplication.class, args);
+	}
+
+}
---
+++ HelperDataApi/src/test/java/com/example/HelperDataApi/HelperDataApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HelperDataApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelperDataApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
